5765a42d1ef05703147ef2badea8bb473ef460bc
Revaz Mustoyan <revaz.mustoyan@yandex.ru>
2024-10-28 12:30:26 +0300

    v2.2

--- url.py
+++
@@ -1,9 +0,0 @@
-class Urls:
-
-    BASE_URL = 'https://stellarburgers.nomoreparties.site'
-    CREATE_USER_ENDPOINT = '/api/auth/register'
-    DELETE_USER_ENDPOINT = '/api/auth/user'
-    LOGIN_ENDPOINT = '/api/auth/login'
-    CHANGE_OR_GET_USER_ENDPOINT = '/api/auth/user'
-    GET_USER_ORDERS_ENDPOINT = '/api/orders'
-    CREATE_ORDER_ENDPOINT = '/api/orders'
